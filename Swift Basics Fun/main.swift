43f2c070bc1bdc3bbfffacf15191912384da4a68
Gina Sprint <sprint@gonzaga.edu>
2020-09-11 10:37:18 -0700

    End of class today almost done with functions

--- Swift Basics Fun/main.swift
+++ Swift Basics Fun/main.swift
@@ -131,3 +131,52 @@ else {
 if myOptionalInt != nil {
     myInt = myOptionalInt!
 }
+
+//var friends = ["Chandler", "Monica", "Ross", "Rachel"]
+//print(friends)
+//print(friends.count)
+//friends.insert("Phoebe", at: 1)
+//print(friends)
+//print("Please enter a friend: ", terminator: "")
+//let inputStringOptional = readLine()
+//if let inputString = inputStringOptional {
+//    print("trying to remove: " + inputString)
+//    let optionalIndex = friends.index(of: inputString)
+//    if let myIndex = optionalIndex {
+//        friends.remove(at: myIndex)
+//        print(friends)
+//    }
+//}
+
+// MARK: - Functions
+// no params, no return
+func sayHello() {
+    print("hello")
+}
+// in main.swift can you have executable code outside of a function
+sayHello()
+
+// one param, no return
+// message is both the internal and external parameter name
+func say(message: String) {
+    print(message)
+}
+say(message: "hola") // message is the argument label (external param name)
+// calling a function should read like an english sentence
+
+// task 4: define/call a function that accepts a radius
+// and prints the area of a circle
+func computeCircleArea(radius: Double) {
+    let area = Double.pi * pow(radius, 2)
+    print(area)
+}
+computeCircleArea(radius: 5.0)
+
+// one param, with return value
+// we can have different external param... e.g. withRadius
+// _ is the anonymous variable
+func computeCircleArea2(_ radius: Double) -> Double {
+    let area = Double.pi * pow(radius, 2)
+    return area
+}
+print(computeCircleArea2(5.0))
